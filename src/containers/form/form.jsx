8ffa3e784aec844bdc1c1cae7bedde5aed4be01c
Mo Sharif <mo@mosh-media.com>
2019-07-23 08:04:47 -0700

    Small clean up

--- src/containers/form/form.jsx
+++ src/containers/form/form.jsx
@@ -1,18 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { welcomeAction } from "../../actions/questionsAction";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
-
 import FormBody from "../../components/form/formBody/formBody";
 import FormHeader from "../../components/form/formHeader/formHeader";
-import questionsReducer from "../../reducers/questionsReducer";
-
-/*
- * mapDispatchToProps
- */
-/* let mapDispatchToProps = dispatch => ({
-	welcomeAction: () => dispatch(welcomeAction())
-}); */
 
 /*
  * mapStateToProps
@@ -22,7 +11,7 @@ const mapStateToProps = state => ({ ...state });
 class Form extends Component {
 	result(values) {
 		alert("Submited: " + JSON.stringify(values));
-	};
+	}
 	render() {
 		return (
 			<div className="flex-container">
@@ -30,14 +19,14 @@ class Form extends Component {
 					<FormHeader />
 				</div>
 				<div className="row">
-					<FormBody onSubmit={this.result} questions={this.props.questionsReducer}/>
+					<FormBody
+						onSubmit={this.result}
+						questions={this.props.questionsReducer}
+					/>
 				</div>
 			</div>
 		);
 	}
 }
 
-export default connect(
-	mapStateToProps,
-	// mapDispatchToProps
-)(Form);
+export default connect(mapStateToProps)(Form);
